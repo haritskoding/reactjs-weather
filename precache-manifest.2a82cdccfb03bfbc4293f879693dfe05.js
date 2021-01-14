f5c7489de9102b5781197fba59195c3def12a9a9
haritskoding <haritscoding@gmail.com>
2021-01-15 06:02:25 +0700

    Updates

---
+++ precache-manifest.2a82cdccfb03bfbc4293f879693dfe05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd069b9b18718fcc54d278124690e1c",
+    "url": "/teguh-reactjs-weather/index.html"
+  },
+  {
+    "revision": "6086be4fc23b92c3db80",
+    "url": "/teguh-reactjs-weather/static/css/main.e5f90743.chunk.css"
+  },
+  {
+    "revision": "6c20a4a2b0ec0e44f361",
+    "url": "/teguh-reactjs-weather/static/js/2.fa31a1e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teguh-reactjs-weather/static/js/2.fa31a1e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6086be4fc23b92c3db80",
+    "url": "/teguh-reactjs-weather/static/js/main.a0c2c63c.chunk.js"
+  },
+  {
+    "revision": "7b9657d5ba38ef2e63d7",
+    "url": "/teguh-reactjs-weather/static/js/runtime-main.55e456c5.js"
+  }
+]);
